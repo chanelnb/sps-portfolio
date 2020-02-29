40c7b448e5ded9604a9406c86b53d14fd92eaa8f
Chanel <chanelnb1@gmail.com>
2020-02-29 01:35:51 -0500

    part 2

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,4 +11,43 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+/**
+ * Fetches a random quote from the server and adds it to the DOM.
+ */
+function getDataServlet() {
+  console.log('Fetching a Hello Chanel.');
+
+  const responsePromise = fetch('/data');
+
+  responsePromise.then(handleResponse);
+}
+
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  const textPromise = response.text();
+
+  textPromise.then(addDataServletToDom);
+}
+
+function addDataServletToDom(texts) {
+  console.log('Adding quote to dom: ' + texts);
+
+  const textsContainer = document.getElementById('dataServlet-container');
+  textsContainer.innerHTML= texts;
+}
+
+
+function getTextUsingArrowFunctions() {
+  fetch('/data').then(response => response.text()).then((texts) => {
+    document.getElementById('dataServlet-container').innerHTML = texts;
+  });
+}
+
+
+async function getTextUsingAsyncAwait() {
+  const response = await fetch('/data');
+  const texts = await response.text();
+  document.getElementById('dataServlet-container').innerHTML = texts;
+}
 
